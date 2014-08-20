7ae24f3a9225297e6c951a82d4be99e516ddaf1d
Chris Warburton <chriswarbo@gmail.com>
2014-08-20 08:22:44 +0100

    Updated tests

--- test.py
+++ test.py
@@ -6,35 +6,48 @@ test = lambda t: t(*map(random, range(t.func_code.co_argcount)))
 
 def failures(ts):
     results = {n: test(t) for n, t in ts.iteritems()}
-    return {n: r for n, r in results.iteritems() if r}
+    return {n: (l, r) for n, (l, r) in results.iteritems() if l != r}
 
 from core import *
 
 core_fails = failures({
     'currying curries': lambda a, b: (
-        curry(lambda x, y: x + y)(a)(b) != a + b),
+        curry(lambda x, y: x + y)(a)(b),
+        a + b),
     'curried functions still callable as normal': lambda a, b: (
-        curry(lambda x, y: x + y)(a, b) != a + b),
+        curry(lambda x, y: x + y)(a, b),
+        a + b),
     'currying uncurries': lambda a, b, c: (
-        curry(lambda x, y: lambda z: x + y + z)(a, b, c) != a + b + c),
+        curry(lambda x, y: lambda z: x + y + z)(a, b, c),
+        a + b + c),
     'can curry types': lambda a: (
-        curry(str)(a) != str(a)),
+        curry(str)(a),
+        str(a)),
     'curry passes on keywords': lambda a, b: (
-        curry(lambda a, **kwargs: [str(x) for x in kwargs.iteritems()],
-              foo=a)(b) != [str(('foo', a))])
+        curry(lambda x, **kwargs: map(str, kwargs.iteritems()),
+              foo=a)(b),
+        [str(('foo', a))]),
+    'curry ignores defaults': lambda a, b, c: (
+        curry(lambda x, y=a: x + y, b, c),
+        b + c)
 })
 
 from plumb import *
 
 plumb_fails = failures({
     'plumb thunks': lambda a, b: (
-        plumb([str(a)])(b) != str(a)),
+        plumb([str(a)])(b),
+        str(a)),
     'plumb id': lambda a: (
-        plumb([0])(a) != a),
+        plumb([0])(a),
+        a),
     'plumb de Bruijn': lambda a: (
-        plumb([[1 , 0]])(str, a) != str(a)),
+        plumb([[1 , 0]])(str, a),
+        str(a)),
     'plumb curries': lambda a, b: (
-        plumb([[[2 , 1 , 0]]])(lambda x, y: x + y, a, b) != a + b),
+        plumb([[[2 , 1 , 0]]])(lambda x, y: x + y, a, b),
+        a + b),
     'plumb uncurries': lambda a: (
-        plumb([str , 0], a) != str(a))
+        plumb([str , 0], a),
+        str(a))
 })
